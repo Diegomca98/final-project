627e41a9c5cfcc96694cebd46205e79df5d2a226
Diegomca98 <diegomca98@gmail.com>
2024-03-27 17:07:58 -0600

    Improving html and adding new templates/views for model deploy

--- src/app.py
+++ src/app.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 import os
+from pickle import load
 
 app = Flask(__name__)
 
-#df = pd.read_csv('../data/raw/test-webapp.csv')
-# stats = {
-#     'mean': df.ArrTime.mean(),
-#     'std': df.ArrTime.std(),
-#     'min': df.ArrTime.min(),
-#     'max': df.ArrTime.max(),
-#     '25': df.ArrTime.quantile(0.25),
-#     '50': df.ArrTime.quantile(0.50),
-#     '75': df.ArrTime.quantile(0.75),
-# }
+# For model_deploy view
+"""
+model = load(
+    open(
+        '<model_route>/model_name.sav', 
+        'rb'
+    )
+)
+class_dict = {
+    '0': 'Early',
+    '1': 'On-Time',
+    '2': 'Late',
+}
+"""
+
+# For model_deploy view
 
 @app.route('/')
 def index():
@@ -57,5 +64,40 @@ def eda_data():
     return render_template(
         'single.html',
         title = 'Fly Me! - Exploratory Data Analysis',
-        visuals = graphs
+        visuals = graphs,
+        banner_title = 'Exploratory Data Analysis',
+        breadcrumb = 'EDA',
+        view_name = 'eda_data'
+    )
+
+@app.route('/model', methods=['GET', 'POST'])
+def model_deploy():
+
+    """
+    val1 = float(request.form['val1'])
+    val2 = float(request.form['val2'])
+    val3 = float(request.form['val3'])
+    val4 = float(request.form['val4'])
+    val5 = float(request.form['val5'])
+
+    data = [
+        [
+            val1,
+            val2,
+            val3,
+            val4,
+            val5
+        ]
+    ]
+    prediction = str(model.predict(data)[0])
+    pred_class = class_dict[prediction]
+    """
+
+    return render_template(
+        'model.html',
+        title = 'Fly Me! - Model',
+        visuals = os.listdir('./static/graphs'),
+        banner_title = 'Try Out Our Model',
+        breadcrumb = 'MODEL',
+        view_name = 'model_deploy'
     )
